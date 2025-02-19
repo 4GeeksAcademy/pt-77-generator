fd73123bc661a8331fa87133790db5a180cdf3d8
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-02-19 23:38:04 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateGame()
 };
+const generateGame = () => {
+  const equipmentArray = ["racket", "bat", "skates", "paddle", "board", "ball", "disk"];
+  const locationArray = ["field", "court", "yard", "park", "beach", "pool", "table"];
+  const nameArray = ["soccer", "compative fast walking", "corn hole", "volley ball", "ping pong", "pole vaulting"];
+
+  let equipmentIndex = Math.floor(Math.random() * equipmentArray.length)
+  let locationIndex  = Math.floor(Math.random() * locationArray.length)
+  let nameIndex  = Math.floor(Math.random() * nameArray.length)
+
+  const equipment = document.querySelector(".equipment")
+  const location = document.querySelector(".location")
+  const name = document.querySelector(".name")
+
+  equipment.innerHTML = equipmentArray[equipmentIndex]
+  location.innerHTML = locationArray[locationIndex]
+  name.innerHTML = nameArray[nameIndex]
+
+}
